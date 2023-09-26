5a4ad48f69d865685234130cb49151d31cdd4742
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-26 18:00:07 +0300

    created function add to card

--- js/main.js
+++ js/main.js
@@ -58,3 +58,66 @@ window.addEventListener('click', function (event) {
 })
 
 // end caunter
+
+// add product to carzine / start
+
+const cartWrapper = document.querySelector('.cart-wrapper')
+
+window.addEventListener('click', function (event) {
+
+    // 
+    if (event.target.hasAttribute('data-cart')) {
+        // console.log('Click in cart btn');
+
+        const card = event.target.closest('.card')
+        // console.log(card);
+
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.product-img').getAttribute('src'),
+            title: card.querySelector('.item-title').innerText,
+            itemInBox: card.querySelector('[data-items-in-box]').innerText,
+            weight: card.querySelector('.price__weight').innerText,
+            price: card.querySelector('.price__currency').innerText,
+            counter: card.querySelector('[data-counter]').innerText,
+        }
+
+        console.log(productInfo);
+
+        const cardItemHTML = `<div class="cart-item" data-id="02">
+        <div class="cart-item__top">
+          <div class="cart-item__img">
+            <img src="img/roll/california-tempura.jpg" alt="" />
+          </div>
+          <div class="cart-item__desc">
+            <div class="cart-item__title">${productInfo.title}</div>
+            <div class="cart-item__weight">${productInfo.itemInBox}/ ${productInfo.weight}</div>
+
+            <!-- cart-item__details -->
+            <div class="cart-item__details">
+              <div class="items items--small counter-wrapper">
+                <div class="items__control" data-action="minus">
+                  -
+                </div>
+                <div class="items__current" data-counter="">${productInfo.counter}</div>
+                <div class="items__control" data-action="plus">+</div>
+              </div>
+
+              <div class="price">
+                <div class="price__currency">${productInfo.price}</div>
+              </div>
+            </div>
+            <!-- // cart-item__details -->
+          </div>
+        </div>
+      </div>`
+
+        //   add to carzine
+
+        cartWrapper.insertAdjacentHTML('beforeend', cardItemHTML)
+
+    }
+})
+
+
+// add product to carzine / end
